83c6f262478b6fcd2e93025de2f65b1fd6fa56bb
Ярослав Шкодин <aroslav882@outlook.com>
2026-04-02 20:40:35 +0400

    Добавьте файлы проекта.

---
+++ FibonacciRepositoy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FibonacciRepositoy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
