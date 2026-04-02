f88bebe8479439dc4f46dbe98ffd2c3aab431345
Ярослав Шкодин <aroslav882@outlook.com>
2026-04-02 20:45:03 +0400

    Change sequence to (0,1)

--- FibonacciRepositoy/Program.cs
+++ FibonacciRepositoy/Program.cs
@@ -4,9 +4,11 @@
     {
         static int Fib(int n)
         {
-            if (n <= 2) 
+            if (n == 1)
+                return 0;  
+            
+            if (n == 2) 
                 return 1;   
-
             return Fib(n - 1) + Fib(n - 2);
         }
         static void Main(string[] args)
