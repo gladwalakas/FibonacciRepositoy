d9d2db39f6673cd7de88c827d893a4f863b61d21
Ярослав Шкодин <aroslav882@outlook.com>
2026-04-02 20:44:13 +0400

    Add recursive fib starting with (1,1)

--- FibonacciRepositoy/Program.cs
+++ FibonacciRepositoy/Program.cs
@@ -2,9 +2,17 @@
 {
     internal class Program
     {
+        static int Fib(int n)
+        {
+            if (n <= 2) 
+                return 1;   
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 10;
+            Console.WriteLine($"Fib({n}) = {Fib(n)}");
         }
     }
 }
