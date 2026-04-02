6a354d8ea0ef8d54f8b38527c25493ae9a04d478
Ярослав Шкодин <aroslav882@outlook.com>
2026-04-02 20:47:27 +0400

    Iterative fib, print first n numbers

--- FibonacciRepositoy/Program.cs
+++ FibonacciRepositoy/Program.cs
@@ -5,16 +5,27 @@
         static int Fib(int n)
         {
             if (n == 1)
-                return 0;  
-            
-            if (n == 2) 
-                return 1;   
-            return Fib(n - 1) + Fib(n - 2);
-        }
-        static void Main(string[] args)
-        {
-            int n = 10;
-            Console.WriteLine($"Fib({n}) = {Fib(n)}");
+                return 0;
+
+            if (n == 2)
+                return 1;
+
+            int prev = 0;
+            int curr = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                int next = prev + curr;
+                prev = curr;
+                curr = next;
+            }
+            return curr;
+
+            static void Main(string[] args)
+            {
+                int n = 10;
+                for (int i = 1; i <= n; i++)
+                    Console.WriteLine(Fib(i));
+            }
         }
     }
 }
